90ac890f59eae3ec88e58fe8da9bf26d6bf41834
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 21:27:55 +0530

    Quo_rem

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Quotiend_remainder
+{
+    class Program
+    {
+        public void Calculation()
+        {
+            int DIVIDENT;
+            int DIVISOR;
+            int REMAINDER;
+            int QUOTIENT;
+            Console.WriteLine("enter the divident :");
+            DIVIDENT =int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the divisor :");
+            DIVISOR = int.Parse(Console.ReadLine());
+            QUOTIENT = DIVIDENT / DIVISOR;
+            REMAINDER = DIVIDENT % DIVISOR;
+            Console.WriteLine("Quotient value : " + QUOTIENT);
+            Console.WriteLine("Remainder value : " + REMAINDER);
+        }
+    }
+    class Result
+    {
+        public static void Main(string[]args)
+        {
+            Program program = new Program();
+            program.Calculation();
+        }
+    }
+}
